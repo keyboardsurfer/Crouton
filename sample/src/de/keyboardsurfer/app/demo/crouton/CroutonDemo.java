ff7d69bb1ac2c221df7795800be602c0644869e6
Artur Dryomov <artur.dryomov@gmail.com>
2012-08-28 22:55:23 +0300

    Refactor Crouton demo activity.

--- sample/src/de/keyboardsurfer/app/demo/crouton/CroutonDemo.java
+++ sample/src/de/keyboardsurfer/app/demo/crouton/CroutonDemo.java
@@ -15,6 +15,7 @@
  */
 package de.keyboardsurfer.app.demo.crouton;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,39 +24,52 @@ import android.widget.EditText;
 import de.neofonie.mobile.app.android.widget.crouton.Crouton;
 import de.neofonie.mobile.app.android.widget.crouton.Style;
 
-public class CroutonDemo extends Activity implements OnClickListener {
-	private EditText duration, text;
 
-	/** Called when the activity is first created. */
+public class CroutonDemo extends Activity implements OnClickListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
 		findViewById(R.id.button_show).setOnClickListener(this);
-		duration = (EditText) findViewById(R.id.edit_text_duration);
-		text = (EditText) findViewById(R.id.edit_text_text);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.view.View.OnClickListener#onClick(android.view.View)
-	 */
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.button_show:
-			final int durationValue = Integer.parseInt(duration.getText()
-					.toString());
-			final String textValue = text.getText().toString();
-			// Display the Crouton with a custom style
-			Crouton.makeText(this, textValue,
-					new Style.Builder().setDuration(durationValue).build())
-					.show();
-			break;
+			case R.id.button_show:
+				showCrouton();
+				break;
 
-		default:
-			break;
+			default:
+				break;
 		}
+	}
 
+	private void showCrouton() {
+		int croutonDuration = readCroutonDuration();
+		String croutonText = readCroutonText();
+
+		// Display the Crouton with a custom style
+		Crouton.makeText(this, croutonText,
+			new Style.Builder().setDuration(croutonDuration).build()).show();
+	}
+
+	private int readCroutonDuration() {
+		EditText croutonDurationEdit = (EditText) findViewById(
+			R.id.edit_text_duration);
+
+		try {
+			String croutonDuration = croutonDurationEdit.getText().toString();
+			return Integer.parseInt(croutonDuration);
+		}
+		catch (NumberFormatException e) {
+			return 0;
+		}
+	}
+
+	private String readCroutonText() {
+		EditText croutonTextEdit = (EditText) findViewById(R.id.edit_text_text);
+
+		return croutonTextEdit.getText().toString();
 	}
 }
