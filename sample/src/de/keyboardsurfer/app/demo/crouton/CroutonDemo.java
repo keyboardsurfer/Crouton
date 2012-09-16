4f4aebb67f72e9d47d14196360fe102f049fffab
Artur Dryomov <artur.dryomov@gmail.com>
2012-09-16 23:58:23 +0300

    Change layout of demo app activity.
    * Remove container for hidden duration field.
    * Set common padding for inner linear layout to fix padding lags on setting
      custom crouton style.

--- sample/src/de/keyboardsurfer/app/demo/crouton/CroutonDemo.java
+++ sample/src/de/keyboardsurfer/app/demo/crouton/CroutonDemo.java
@@ -136,15 +136,15 @@ public class CroutonDemo extends Activity implements OnClickListener, OnItemSele
 	}
 
 	public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-		View customCroutonControls = findViewById(R.id.container_custom);
+		EditText croutonDurationEdit = (EditText) findViewById(R.id.edit_text_duration);
 
 		switch ((int) id) {
 			case 3:
-				customCroutonControls.setVisibility(View.VISIBLE);
+				croutonDurationEdit.setVisibility(View.VISIBLE);
 				break;
 
 			default:
-				customCroutonControls.setVisibility(View.GONE);
+				croutonDurationEdit.setVisibility(View.GONE);
 				break;
 		}
 	}
