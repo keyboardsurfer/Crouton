facd9f27de7eaa676d3d14b8c8ff337050de5d9d
keyboardsurfer <keyboardsurfer@c-base.org>
2012-10-14 22:28:29 +0200

    Lazy initalization for default animations

--- library/src/de/neofonie/mobile/app/android/widget/crouton/AnimationsBuilder.java
+++ library/src/de/neofonie/mobile/app/android/widget/crouton/AnimationsBuilder.java
@@ -21,7 +21,9 @@ import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 
 final class AnimationsBuilder {
-  
+
+  private static Animation slideInDownAnimation, slideOutUpAnimation;
+
   private static final class SlideInDownAnimationParameters {
     private SlideInDownAnimationParameters() {
     }
@@ -50,18 +52,21 @@ final class AnimationsBuilder {
   }
 
   public static Animation buildSlideInDownAnimation() {
-    Animation slideInDownAnimation = new TranslateAnimation(SlideInDownAnimationParameters.FROM_X_DELTA, SlideInDownAnimationParameters.TO_X_DELTA,
-        SlideInDownAnimationParameters.FROM_Y_DELTA, SlideInDownAnimationParameters.TO_Y_DELTA);
-    slideInDownAnimation.setDuration(SlideInDownAnimationParameters.DURATION);
+    if (slideInDownAnimation == null) {
+      slideInDownAnimation = new TranslateAnimation(SlideInDownAnimationParameters.FROM_X_DELTA, SlideInDownAnimationParameters.TO_X_DELTA,
+          SlideInDownAnimationParameters.FROM_Y_DELTA, SlideInDownAnimationParameters.TO_Y_DELTA);
+      slideInDownAnimation.setDuration(SlideInDownAnimationParameters.DURATION);
+    }
 
     return slideInDownAnimation;
   }
 
   public static Animation buildSlideOutUpAnimation() {
-    Animation slideOutUpAnimation = new TranslateAnimation(SlideOutUpAnimationParameters.FROM_X_DELTA, SlideOutUpAnimationParameters.TO_X_DELTA,
-        SlideOutUpAnimationParameters.FROM_Y_DELTA, SlideOutUpAnimationParameters.TO_Y_DELTA);
-    slideOutUpAnimation.setDuration(SlideOutUpAnimationParameters.DURATION);
-
+    if (slideOutUpAnimation == null) {
+      slideOutUpAnimation = new TranslateAnimation(SlideOutUpAnimationParameters.FROM_X_DELTA, SlideOutUpAnimationParameters.TO_X_DELTA,
+          SlideOutUpAnimationParameters.FROM_Y_DELTA, SlideOutUpAnimationParameters.TO_Y_DELTA);
+      slideOutUpAnimation.setDuration(SlideOutUpAnimationParameters.DURATION);
+    }
     return slideOutUpAnimation;
   }
 }
