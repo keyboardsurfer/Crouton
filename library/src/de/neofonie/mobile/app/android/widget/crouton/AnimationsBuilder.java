135cd18ee65f968e1383ec2911e4ad98b053c5c7
Artur Dryomov <artur.dryomov@gmail.com>
2012-10-14 22:11:26 +0300

    Change tabs to spaces in animations builder.

--- library/src/de/neofonie/mobile/app/android/widget/crouton/AnimationsBuilder.java
+++ library/src/de/neofonie/mobile/app/android/widget/crouton/AnimationsBuilder.java
@@ -7,50 +7,50 @@ import android.view.animation.TranslateAnimation;
 
 final class AnimationsBuilder
 {
-	private static final class SlideInDownAnimationParameters
-	{
-		private SlideInDownAnimationParameters() {
-		}
+  private static final class SlideInDownAnimationParameters
+  {
+    private SlideInDownAnimationParameters() {
+    }
 
-		public static final float FROM_X_DELTA = 0;
-		public static final float TO_X_DELTA = 0;
-		public static final float FROM_Y_DELTA = -50;
-		public static final float TO_Y_DELTA = 0;
+    public static final float FROM_X_DELTA = 0;
+    public static final float TO_X_DELTA = 0;
+    public static final float FROM_Y_DELTA = -50;
+    public static final float TO_Y_DELTA = 0;
 
-		public static final long DURATION = 400;
-	}
+    public static final long DURATION = 400;
+  }
 
-	private static final class SlideOutUpAnimationParameters
-	{
-		private SlideOutUpAnimationParameters() {
-		}
+  private static final class SlideOutUpAnimationParameters
+  {
+    private SlideOutUpAnimationParameters() {
+    }
 
-		public static final float FROM_X_DELTA = 0;
-		public static final float TO_X_DELTA = 0;
-		public static final float FROM_Y_DELTA = 0;
-		public static final float TO_Y_DELTA = -50;
+    public static final float FROM_X_DELTA = 0;
+    public static final float TO_X_DELTA = 0;
+    public static final float FROM_Y_DELTA = 0;
+    public static final float TO_Y_DELTA = -50;
 
-		public static final long DURATION = 400;
-	}
+    public static final long DURATION = 400;
+  }
 
-	private AnimationsBuilder() {
-	}
+  private AnimationsBuilder() {
+  }
 
-	public static Animation buildSlideInDownAnimation() {
-		Animation slideInDownAnimation = new TranslateAnimation(
-			SlideInDownAnimationParameters.FROM_X_DELTA, SlideInDownAnimationParameters.TO_X_DELTA,
-			SlideInDownAnimationParameters.FROM_Y_DELTA, SlideInDownAnimationParameters.TO_Y_DELTA);
-		slideInDownAnimation.setDuration(SlideInDownAnimationParameters.DURATION);
+  public static Animation buildSlideInDownAnimation() {
+    Animation slideInDownAnimation = new TranslateAnimation(
+      SlideInDownAnimationParameters.FROM_X_DELTA, SlideInDownAnimationParameters.TO_X_DELTA,
+      SlideInDownAnimationParameters.FROM_Y_DELTA, SlideInDownAnimationParameters.TO_Y_DELTA);
+    slideInDownAnimation.setDuration(SlideInDownAnimationParameters.DURATION);
 
-		return slideInDownAnimation;
-	}
+    return slideInDownAnimation;
+  }
 
-	public static Animation buildSlideOutUpAnimation() {
-		Animation slideOutUpAnimation = new TranslateAnimation(
-			SlideOutUpAnimationParameters.FROM_X_DELTA, SlideOutUpAnimationParameters.TO_X_DELTA,
-			SlideOutUpAnimationParameters.FROM_Y_DELTA, SlideOutUpAnimationParameters.TO_Y_DELTA);
-		slideOutUpAnimation.setDuration(SlideOutUpAnimationParameters.DURATION);
+  public static Animation buildSlideOutUpAnimation() {
+    Animation slideOutUpAnimation = new TranslateAnimation(
+      SlideOutUpAnimationParameters.FROM_X_DELTA, SlideOutUpAnimationParameters.TO_X_DELTA,
+      SlideOutUpAnimationParameters.FROM_Y_DELTA, SlideOutUpAnimationParameters.TO_Y_DELTA);
+    slideOutUpAnimation.setDuration(SlideOutUpAnimationParameters.DURATION);
 
-		return slideOutUpAnimation;
-	}
+    return slideOutUpAnimation;
+  }
 }
