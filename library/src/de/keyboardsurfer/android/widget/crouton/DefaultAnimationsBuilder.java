795d65f8c8cbefb5897d6cbe76c7bae1ef001706
keyboardsurfer <keyboardsurfer@c-base.org>
2013-04-28 18:25:35 +0200

    caching the animation if possible

--- library/src/de/keyboardsurfer/android/widget/crouton/DefaultAnimationsBuilder.java
+++ library/src/de/keyboardsurfer/android/widget/crouton/DefaultAnimationsBuilder.java
@@ -25,6 +25,7 @@ import android.view.animation.TranslateAnimation;
 final class DefaultAnimationsBuilder {
   private static final long DURATION = 400;
   private static Animation slideInDownAnimation, slideOutUpAnimation;
+  private static int lastInAnimationHeight, lastOutAnimationHeight;
 
   private DefaultAnimationsBuilder() {
     /* no-op */
@@ -37,11 +38,14 @@ final class DefaultAnimationsBuilder {
    * @return The default Animation for a showing {@link Crouton}.
    */
   static Animation buildDefaultSlideInDownAnimation(View croutonView) {
-    slideInDownAnimation = new TranslateAnimation(
-      0, 0,                               // X: from, to
-      -croutonView.getMeasuredHeight(), 0 // Y: from, to
-    );
-    slideInDownAnimation.setDuration(DURATION);
+    if (!areLastMeasuredInAnimationHeightAndCurrentEqual(croutonView) || (null == slideInDownAnimation)) {
+      slideInDownAnimation = new TranslateAnimation(
+        0, 0,                               // X: from, to
+        -croutonView.getMeasuredHeight(), 0 // Y: from, to
+      );
+      slideInDownAnimation.setDuration(DURATION);
+      setLastInAnimationHeight(croutonView.getMeasuredHeight());
+    }
     return slideInDownAnimation;
   }
 
@@ -52,11 +56,34 @@ final class DefaultAnimationsBuilder {
    * @return The default Animation for a hiding {@link Crouton}.
    */
   static Animation buildDefaultSlideOutUpAnimation(View croutonView) {
-    slideOutUpAnimation = new TranslateAnimation(
-      0, 0,                               // X: from, to
-      0, -croutonView.getMeasuredHeight() // Y: from, to
-    );
-    slideOutUpAnimation.setDuration(DURATION);
+    if (!areLastMeasuredOutAnimationHeightAndCurrentEqual(croutonView) || (null == slideOutUpAnimation)) {
+      slideOutUpAnimation = new TranslateAnimation(
+        0, 0,                               // X: from, to
+        0, -croutonView.getMeasuredHeight() // Y: from, to
+      );
+      slideOutUpAnimation.setDuration(DURATION);
+      setLastOutAnimationHeight(croutonView.getMeasuredHeight());
+    }
     return slideOutUpAnimation;
+  }
+
+  private static boolean areLastMeasuredInAnimationHeightAndCurrentEqual(View croutonView) {
+    return areLastMeasuredAnimationHeightAndCurrentEqual(lastInAnimationHeight, croutonView);
+  }
+
+  private static boolean areLastMeasuredOutAnimationHeightAndCurrentEqual(View croutonView) {
+    return areLastMeasuredAnimationHeightAndCurrentEqual(lastOutAnimationHeight, croutonView);
+  }
+
+  private static boolean areLastMeasuredAnimationHeightAndCurrentEqual(int lastHeight, View croutonView) {
+    return lastHeight == croutonView.getMeasuredHeight();
+  }
+
+  private static void setLastInAnimationHeight(int lastInAnimationHeight) {
+    DefaultAnimationsBuilder.lastInAnimationHeight = lastInAnimationHeight;
+  }
+
+  private static void setLastOutAnimationHeight(int lastOutAnimationHeight) {
+    DefaultAnimationsBuilder.lastOutAnimationHeight = lastOutAnimationHeight;
   }
 }
